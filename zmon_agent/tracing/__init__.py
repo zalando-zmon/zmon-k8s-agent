e6f3ab869bbf75fc93c6c2b461a0ec580999c205
Mohab Usama <mohab.abdelhameed@zalando.de>
2017-09-08 23:28:53 +0200

    Update init_tracer

--- zmon_agent/tracing/__init__.py
+++ zmon_agent/tracing/__init__.py
@@ -1,20 +1,13 @@
 import opentracing
 
-from enum import Enum
-
-
-class Tracers(Enum):
-    BASIC = 'basic'
-    INSTANA = 'instana'
-
 
 def init_opentracing_tracer(tracer, debug_level):
-    if tracer == Tracers.INSTANA:
+    if tracer == 'instana':
         import instana.options as instanaOpts  # noqa
         import instana.tracer  # noqa
 
         instana.tracer.init(instanaOpts.Options(service='zmon-agent', log_level=debug_level))
-    elif tracer == Tracers.BASIC:
+    elif tracer == 'basic':
         from basictracer import BasicTracer  # noqa
 
         opentracing.tracer = BasicTracer()
