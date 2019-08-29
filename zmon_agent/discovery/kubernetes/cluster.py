306ec5aaffab61525a9d2e68c8bf98392dd0182c
Rafia Sabih <rafia.sabih@zalando.de>
2019-08-29 12:06:27 +0200

    Revert "Add pod start time and patroni state for postgresql entities"
    This reverts commit f23333306dced7521ac03d1f779c5ab3416258e3.

--- zmon_agent/discovery/kubernetes/cluster.py
+++ zmon_agent/discovery/kubernetes/cluster.py
@@ -1142,9 +1142,7 @@ def get_postgresql_cluster_members(kube_client, cluster_id, alias, environment, 
                                                          cluster_name, pod.name),
             'icon2': 'fa-line-chart',
             'namespace': pod_namespace,
-            'team_id': labels.get('team'),
-            'start_time': obj.get('status', {}).get('startTime', ''),
-            'patroni_state': obj['metadata'].get('annotations', {}).get('status').partition('state')[2].partition(':')[2].partition(',')[0]
+            'team_id': labels.get('team')
         }
 
         entities.append(entity)
